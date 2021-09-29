a035eb3a4693aad6cd52abefcb1f17a1576e3bb6
OlukaDenis <olukadeno@gmail.com>
2021-09-29 11:13:18 +0300

    :sparkles: Setup eslint

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 // Load env config
-dotenv.config({ path: './config/config.env'})
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello from express');
+});
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ));
+// Listen server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
