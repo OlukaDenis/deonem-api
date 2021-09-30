cb252189ee4fb6999a1db0500d1c0935209ee66d
OlukaDenis <olukadeno@gmail.com>
2021-10-01 01:25:52 +0300

    :boom: Add a seeder to populate data to the atlas db

---
+++ seeder.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+// Looad models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Read JSOJ files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf8'));
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data imported...'.green.inverse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data deleted...'.red.inverse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-o') {
+  deleteData();
+}
